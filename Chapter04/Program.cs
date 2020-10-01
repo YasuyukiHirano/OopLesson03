8f213e29b326d278443341dda8cae4f66dd56900
YasuyukiHirano <31039@ojs.ac.jp>
2020-10-01 10:37:48 +0900

    P117リスト4-28

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +11,30 @@ namespace Chapter04
     {
         static void Main(string[] args)
         {
-            string code = "12345";
-
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
+            Console.WriteLine(GetProduct());
         }
 
-        //スタブ
-        private static object DefaultMessage()
+        private static string GetProduct()
         {
-            return "DefaultMessage"; 
-        }
+            Sale sale = new Sale
+            {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            //sale = null;
 
-        //スタブ
-        private static object GetMessage(string code)
-        {
-            return "GetMessage";
+            //null条件演算子
+            return sale?.Product;
         }
+    }
+
+    class Sale
+    {
+        //店舗名
+        public String ShopName { get; set; }
+        //売上高
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }
