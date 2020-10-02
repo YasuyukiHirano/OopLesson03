141e97cf5d6f743b77028dce3550d6bfad99fc35
YasuyukiHirano <31039@ojs.ac.jp>
2020-10-02 14:02:16 +0900

    演習問題4-1完成

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -11,30 +11,7 @@ namespace Chapter04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetProduct());
+            
         }
-
-        private static string GetProduct()
-        {
-            Sale sale = new Sale
-            {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            //sale = null;
-
-            //null条件演算子
-            return sale?.Product;
-        }
-    }
-
-    class Sale
-    {
-        //店舗名
-        public String ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; }
-        public string Product { get; set; }
     }
 }
---
+++ Chapter04/YearMonth.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04
+{
+    class YearMonth
+    {
+        //演習問題4-1
+        #region 4-1-1
+        //プロパティ
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        //コンストラクタ
+        public YearMonth(int year,int month)
+        {
+            Year = year;
+            Month = month;
+        }
+        #endregion
+
+
+
+        #region 4-1-2
+        //Is21Centuryプロパティを追加
+        public bool Is21Century
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        #endregion
+
+
+
+        #region 4-1-3
+        //AddOneMonth()メソッドを追加
+        public YearMonth AddOneMonth()
+        {
+            if (Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+            else
+            {
+                return new YearMonth(Year,this.Month + 1);
+            }
+        }
+        #endregion
+
+
+
+        #region 4-1-4
+        //ToString()メソッドのオーバーライド
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+        #endregion
+    }
+}
