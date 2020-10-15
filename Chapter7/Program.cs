af9186ec39906f28d3df9092efdf36b26218e8e2
YasuyukiHirano <31039@ojs.ac.jp>
2020-10-15 11:52:07 +0900

    10/15 辞書登録プログラム

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -81,38 +82,102 @@ namespace Chapter7
             ////var md = new MonthDay(8, 4);
             ////var s = dict[md];
             ////Console.WriteLine(s);
-            DuplicateKeySample();
+            //DuplicateKeySample();
             #endregion
-        }
 
-        public static void DuplicateKeySample()
-        {
+            Console.WriteLine("********************");
+            Console.WriteLine("*辞書登録プログラム*");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+
             // ディクショナリの初期化
-            var dict = new Dictionary<string, List<string>>() {
-               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
-               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
-            };
+            var dict = new Dictionary<string, List<string>>() { };            
 
-            // ディクショナリに追加
-            var key = "EC";
-            var value = "電子商取引";
-            if (dict.ContainsKey(key))
-            {
-                dict[key].Add(value);
-            }
-            else
-            {
-                dict[key] = new List<string> { value };
-            }
 
-            // ディクショナリの内容を列挙
-            foreach (var item in dict)
+            while(true)
             {
-                foreach (var term in item.Value)
+                Console.WriteLine("1.登録 2.内容を表示 3.終了");
+                Console.Write(">");
+                var choice = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                switch (choice)
                 {
-                    Console.WriteLine("{0} : {1}", item.Key, term);
+                    case 1:
+                            // ディクショナリに追加
+                            Console.Write("KEYを入力: ");
+                            var key = Console.ReadLine();
+                            Console.Write("VALUEを入力: ");
+                            var value = Console.ReadLine();
+                            if (dict.ContainsKey(key))
+                            {
+                                dict[key].Add(value);
+                            }
+                            else
+                            {
+                                dict[key] = new List<string> { value };
+                            }
+                            Console.WriteLine("登録しました!");
+                        Console.WriteLine("");
+                    break;
+
+                    case 2:
+                        // ディクショナリの内容を列挙
+                        foreach (var item in dict)
+                        {
+                            foreach (var term in item.Value)
+                            {
+                                Console.WriteLine("{0} : {1}", item.Key, term);
+                            }
+                        }
+                    break;
+
+                    case 3:
+                        break;
+
+                    default :
+                        Console.WriteLine("1or2or3を選択してください。");
+                        Console.WriteLine();
+                        break;
+                }
+                if (choice == 3)
+                {
+                    break;
                 }
             }
         }
+
+        #region
+        //public static void DuplicateKeySample()
+        //{
+        //    // ディクショナリの初期化
+        //    var dict = new Dictionary<string, List<string>>() {
+        //       { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+        //       { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+        //    };
+
+        //    // ディクショナリに追加
+        //    var key = "EC";
+        //    var value = "電子商取引";
+        //    if (dict.ContainsKey(key))
+        //    {
+        //        dict[key].Add(value);
+        //    }
+        //    else
+        //    {
+        //        dict[key] = new List<string> { value };
+        //    }
+
+        //    // ディクショナリの内容を列挙
+        //    foreach (var item in dict)
+        //    {
+        //        foreach (var term in item.Value)
+        //        {
+        //            Console.WriteLine("{0} : {1}", item.Key, term);
+        //        }
+        //    }
+        //}
+        #endregion
+
     }
 }
